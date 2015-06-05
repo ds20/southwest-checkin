8b08930c805c6a4939b4b3cea299b406da078d42
Dragos S <sbarlea@gmail.com>
2015-06-05 15:33:17 -0500

    Preparing for heroku - disable user registrations.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
+  def new
+    flash[:info] = 'Registrations are not open yet, because the site is not functional.'
+    redirect_to root_path
+  end
+
+  def create
+    flash[:info] = 'Registrations are not open yet, because the site is not functional.'
+    redirect_to root_path
+  end
+  
   def after_sign_up_path_for(resource)
     '/reservations/new'
   end
